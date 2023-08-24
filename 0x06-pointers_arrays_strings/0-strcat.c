b47f70dcf8682fff0cfedddacfe5e28aa38d37e5
ahmedrafat23 <ahmedrafatdawood@gmail.com>
2023-08-24 02:32:16 -0700

    a

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,40 +1,46 @@
 #include <stdio.h>
 
-/*
+/**
+ * _strcat - Concatenates two strings.
+ * @dest: The destination string.
+ * @src: The source string.
+ *
+ * Return: Pointer to the resulting string dest.
+ */
+char *_strcat(char *dest, char *src)
+{
+	char *ptr = dest;
 
-Write a function that concatenates two strings.
+	while (*ptr != '\0')
+	{
+		ptr++;
+	}
 
-Prototype: char *_strcat(char *dest, char *src);
-This function appends the src string to the dest string, overwriting the terminating null byte (\0) at the end of dest, and then adds a terminating null byte
-Returns a pointer to the resulting string dest
+	while (*src != '\0')
+	{
+		*ptr = *src;
+		ptr++;
+		src++;
+	}
 
-*/
+	*ptr = '\0';
 
-char *_strcat(char *dest, char *src) {
-    char *ptr = dest;
-    
-    while (*ptr != '\0') {
-        ptr++;
-    }
-    
-    while (*src != '\0') {
-        *ptr = *src;
-        ptr++;
-        src++;
-    }
-    
-    *ptr = '\0';
-    
-    return dest;
+	return (dest);
 }
 
-int main() {
-    char dest[50] = "Hello ";
-    char src[] = "World!";
-    
-    printf("%s\n", dest); 
-    printf("%s\n", src);
-    printf("%s\n", _strcat(dest, src));
+/**
+ * main - Entry point.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	char dest[50] = "Hello ";
+	char src[] = "World!";
 
-    return 0;
+	printf("%s\n", dest);
+	printf("%s\n", src);
+	printf("%s\n", _strcat(dest, src));
+
+	return (0);
 }
