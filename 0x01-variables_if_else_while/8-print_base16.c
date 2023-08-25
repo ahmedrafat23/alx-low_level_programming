425f50b87b790c100b5ad2a727a3c85358589ea3
ahmedrafat23 <ahmedrafatdawood@gmail.com>
2023-08-25 01:08:31 -0700

    a

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -10,12 +10,13 @@
 int main(void)
 {
 	int digit = 48; /* 48: decimal representation of '0' */
+	
 	while (digit <= 102) /* 102: decimal representation of 'f' */
 	{
 	putchar(digit);
-	/* After '9', we jump to 'a' */
+	/* After '9', we jump till 96;  '*/
 	if (digit == 57)
-	digit *= 39;
+	digit += 39;
 	digit++;
 	}
 	putchar('\n');
