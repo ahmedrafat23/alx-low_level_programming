11c974ff1f78b4be1f2e1a8723ef97f8383d71fa
ahmedrafat23 <ahmedrafatdawood@gmail.com>
2023-08-25 01:21:39 -0700

    a

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -5,17 +5,16 @@
  *
  * Description: prints _putchar using putchar prototype
  *
- * Return: Alwyas 0 (success)
+ * Return: Always 0 (success)
  */
-
 int main(void)
 {
-	char str{} = "_putchar";
+	char str[] = "_putchar";
 	int ch;
 
 	for (ch = 0; ch < 8; ch++)
-		_putchar(str[ch]);
-	_putchar('\n')
+	_putchar(str[ch]);
+	_putchar('\n');
 
-		return (0);
+	return (0);
 }
