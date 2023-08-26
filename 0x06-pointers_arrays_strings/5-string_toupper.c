22b34a05ef35528fc5e1778bf9b1e0381c5a8f2c
ahmedrafat23 <ahmedrafatdawood@gmail.com>
2023-08-25 18:59:40 -0700

    a

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -12,11 +12,11 @@ char *string_toupper(char *str)
 
 	for (i = 0; str[i] != '\0'; i++)
 	{
-	if (str[i] >= 'a' && str[i] <= 'z')
+	if (str[i] >= 97 && str[i] <= 122)
 	{
 		str[i] = str[i] - 32;
 	}
 	}
 
-	return str;
+	return (str);
 }
