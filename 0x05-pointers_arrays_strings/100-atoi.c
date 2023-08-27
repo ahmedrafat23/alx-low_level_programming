ae74f6c8ed970f42e7527d2714bfab129513d52a
ahmedrafat23 <ahmedrafatdawood@gmail.com>
2023-08-26 22:53:46 -0700

    a

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,13 +1,11 @@
 #include "main.h"
 
 /**
- * _atoi - converts a string to an ineger
- *
- * 0s: string input parameter
+ * _atoi - converts a string to an integer
+ * @s: string input parameter
  *
  * Return: converted integer from string
  */
-
 int _atoi(char *s)
 {
 	unsigned int num = 0;
