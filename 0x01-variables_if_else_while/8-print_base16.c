de7e7dfd4b3f5c5780dd2fb5c1fb0884d1e66031
ahmedrafat23 <ahmedrafatdawood@gmail.com>
2023-08-27 04:25:44 -0700

    a

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -3,21 +3,21 @@
 /**
  * main - Entry point
  *
- * Description: Prints numbers of base16 in lowercase
+ * Description: Prints numbers of base 16 in lowercase
  *
  * Return: Always 0 (success)
  */
 int main(void)
 {
 	int digit = 48; /* 48: decimal representation of '0' */
-	
+
 	while (digit <= 102) /* 102: decimal representation of 'f' */
 	{
-	putchar(digit);
-	/* After '9', we jump till 96;  '*/
-	if (digit == 57)
-	digit += 39;
-	digit++;
+		putchar(digit);
+		/* After '9', we jump till 96 */
+		if (digit == 57)
+			digit += 39;
+		digit++;
 	}
 	putchar('\n');
 
