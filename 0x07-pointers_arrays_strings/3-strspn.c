5b9321de5c57e50abc9478739af0c1c02a8df06c
ahmedrafat23 <ahmedrafatdawood@gmail.com>
2023-08-28 03:03:09 -0700

    a

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,24 +1,24 @@
 #include "main.h"
 
 /**
- * _strspn - gets the length of a perfix substring
+ * _strspn - gets the length of a prefix substring
  *
  * @s: string.
  * @accept: bytes.
  * Return: unsigned int.
  */
-
 unsigned int _strspn(char *s, char *accept)
 {
 	unsigned int itr, jtr;
 
 	for (itr = 0; s[itr] != '\0'; itr++)
 	{
-		for (jtr = 0: accept[jtr] != s[itr]; jtr++)
+		for (jtr = 0; accept[jtr] != s[itr]; jtr++)
 		{
 			if (accept[jtr] == '\0')
-				return
+				return (itr);
 		}
 	}
+
 	return (itr);
 }
