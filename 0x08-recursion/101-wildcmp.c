4183b2d529adb120d47c4ba3c913143f8f43aaa4
ahmedrafat23 <ahmedrafatdawood@gmail.com>
2023-08-31 07:20:20 -0700

    a

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -61,7 +61,7 @@ int wildcmp(char *s1, char *s2)
 			return (1);
 		if (*s1 == *s2)
 			ret += wildcmp(s1 + 1, s2 + 1);
-		ret += inception(s1 + 1, s2 + 1);
+		ret += inception(s1, s2);
 		return (!!ret);
 	}
 	return (0);
