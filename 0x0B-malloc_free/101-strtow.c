11512b9d805b3e1ded9e59c77c68e90967255e8f
ahmedrafat23 <ahmedrafatdawood@gmail.com>
2023-09-05 20:41:22 -0700

    a

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -6,15 +6,15 @@
  *
  * Return: int of number of words
  */
+
 int wrdcnt(char *s)
 {
 	int i, n = 0;
-
 	for (i = 0; s[i]; i++)
 	{
-		if (s[i] == ' ')
+		if (s[i] == '\t')
 		{
-			if (s[i + 1] != ' ' && s[i + 1] != '\0')
+			if (s[i + 1] != '\t' && s[i + 1] != '\0')
 				n++;
 		}
 		else if (i == 0)
@@ -30,35 +30,30 @@ int wrdcnt(char *s)
  *
  * Return: pointer to an array of strings
  */
+
 char **strtow(char *str)
 {
 	int i, j, k, l, n = 0, wc = 0;
 	char **w;
-
 	if (str == NULL || *str == '\0')
 		return (NULL);
-
 	n = wrdcnt(str);
 	if (n == 1)
 		return (NULL);
-
 	w = (char **)malloc(n * sizeof(char *));
 	if (w == NULL)
 		return (NULL);
-
 	w[n - 1] = NULL;
 	i = 0;
 	while (str[i])
 	{
-		if (str[i] != ' ' && (i == 0 || str[i - 1] == ' '))
+		if (str[i] != '\t' && (i == 0 || str[i - 1] == '\t'))
 		{
-			for (j = 1; str[i + j] != ' ' && str[i + j]; j++)
+			for (j = 1; str[i + j] != '\t' && str[i + j]; j++)
 				;
-
 			j++;
 			w[wc] = (char *)malloc(j * sizeof(char));
 			j--;
-
 			if (w[wc] == NULL)
 			{
 				for (k = 0; k < wc; k++)
@@ -67,10 +62,8 @@ char **strtow(char *str)
 				free(w);
 				return (NULL);
 			}
-
 			for (l = 0; l < j; l++)
 				w[wc][l] = str[i + l];
-
 			w[wc][l] = '\0';
 			wc++;
 			i += j;
@@ -78,6 +71,5 @@ char **strtow(char *str)
 		else
 			i++;
 	}
-
 	return (w);
 }
