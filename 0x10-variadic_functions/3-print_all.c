ad68eaf6d6ff20898183c679df03e9185b3a714a
ahmedrafat23 <ahmedrafatdawood@gmail.com>
2023-09-14 13:52:02 -0700

    a

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -40,10 +40,12 @@ void format_string(char *separator, va_list ap)
 	char *str = va_arg(ap, char *);
 
 	switch ((int)(!str))
+	{
 	case 1:
-		str = "(nil)";
-
-		printf("%s%s", separator, str);
+	str = "(nil)";
+	default:
+	printf("%s%s", separator, str);
+	}
 }
 
 /**
@@ -56,28 +58,28 @@ void print_all(const char * const format, ...)
 	char *separator = "";
 	va_list ap;
 	token_t tokens[] = {
-		{"c", format_char},
-		{"i", format_int},
-		{"f", format_float},
-		{"s", format_string},
-		{NULL, NULL}
+	{"c", format_char},
+	{"i", format_int},
+	{"f", format_float},
+	{"s", format_string},
+	{NULL, NULL}
 	};
 
 	va_start(ap, format);
 	while (format && format[i])
 	{
-		j = 0;
-		while (tokens[j].token)
-		{
-			if (format[i] == tokens[j].token[0])
-			{
-				tokens[j].f(separator, ap);
-				separator = ", ";
-			}
-			j++;
-		}
-		i++;
+	j = 0;
+	while (tokens[j].token)
+	{
+	if (format[i] == tokens[j].token[0])
+	{
+	tokens[j].f(separator, ap);
+	separator = ", ";
 	}
-	print("\n");
+	j++;
+	}
+	i++;
+	}
+	printf("\n");
 	va_end(ap);
 }
