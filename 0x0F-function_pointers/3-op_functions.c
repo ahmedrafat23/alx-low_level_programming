688949760c6c8b4f658b8a5974e9b3dcb83c24e4
ahmedrafat23 <ahmedrafatdawood@gmail.com>
2023-09-14 03:02:08 -0700

    a

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,37 @@
+#include "3-calc.h"
+
+/**
+ * op_add - adds two inegers
+ * @a: the first int
+ * @b: the second int
+ *
+ * Return: the sum
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - subtracts two ingegers
+ * @a: the first int
+ * @b: the second int
+ *
+ * Return: the fifference
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_div - divides two integers
+ * @a: the first int
+ * @b: the second int
+ *
+ * Return: the quotient
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
