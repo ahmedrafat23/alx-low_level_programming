2ac63f28283c2c06b2870dcaf56a2dfb3ddfadaf
ahmedrafat23 <ahmedrafatdawood@gmail.com>
2023-09-19 04:55:46 -0700

    a

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -11,11 +11,10 @@ void free_dog(dog_t *d)
 {
 	if (d)
 	{
-
-		if (d(->name)
-				free(d->name);
-				if (d-owner)
-				free(d->owner);
-				free(d);
-				}
-				}
+	if (d->name)
+	free(d->name);
+	if (d->owner)
+	free(d->owner);
+	free(d);
+	}
+}
