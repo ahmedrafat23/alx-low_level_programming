835d18680788563d7c75764877b7a405d2bd017e
ahmedrafat23 <ahmedrafatdawood@gmail.com>
2023-09-22 11:39:46 -0700

    a

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,14 +1,19 @@
-i#include "lists.h"
-
-void _constructor(void) __attribute__ ((constructor));
+#include <stdlib.h>
+#include "lists.h"
 
 /**
- * _constructor - executes before main()
- *
- * Return: void
+ * free_list - frees a linked list
+ * @head: list_t list to be freed
  */
-void _constructor(void)
+void free_list(list_t *head)
 {
-	printf("You 're beat!and yet, you must allow,\n"
-			"I bore my house upon my back!\n");
+	list_t *temp;
+
+	while (head)
+	{
+		temp = head->next;
+		free(head->str);
+		free(head);
+		head = temp;
+	}
 }
